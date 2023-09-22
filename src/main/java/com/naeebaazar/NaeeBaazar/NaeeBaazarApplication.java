c86cdec32889f568d6ac942170d4d95e0bdecce1
Avinash Kumar <avinash_kumar3@persistent.com>
2023-09-22 13:25:32 +0530

    first commit

---
+++ src/main/java/com/naeebaazar/NaeeBaazar/NaeeBaazarApplication.java
@@ -0,0 +1,13 @@
+package com.naeebaazar.NaeeBaazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaeeBaazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaeeBaazarApplication.class, args);
+	}
+
+}
